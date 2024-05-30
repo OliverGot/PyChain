739aa32cd70eb07d477466c197aa1da0f8ba82bd
Oliver Gotzinger <oliver.got@icloud.com>
2024-05-30 12:16:19 +1000

    finished blockchian file

--- blockchain.py
+++ blockchain.py
@@ -1,32 +1,100 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from hashlib import sha256
 
 def updateHash(*args):
-    hashingText = ""; h = sha256()
+    hashing_text = ""; h = sha256()
     for arg in args:
-        hashingText += str(args)
-    
-    h.update(hashingText.encode("utf-8"))
+        hashing_text += str(arg)
+        
+    h.update(hashing_text.encode('utf-8'))
     return h.hexdigest()
 
-class Block():
-    data = None
-    hash = None
-    nonce = 0
-    previousHash = "0" * 64
+print(updateHash("Hello World"))
 
-    def __init__(self, data, number=0):
+class Block():
+
+    def __init__(self, data=None, number=0, previous_hash="0"*64,  nonce=0):
         self.data = data
         self.number = number
+        self.previous_hash = previous_hash
+        self.nonce = nonce
     
     def hash(self):
-       return updateHash(self.previousHash, self.number, self.data, self.nonce)
-
+        return updateHash(
+            self.previous_hash, 
+            self.number, 
+            self.data, 
+            self.nonce
+        )
+    
+    def __str__(self):
+        return str("Block#: %s\nHash: %s\nPrevious: %s\nData: %s\nNonce: %s\n" %(
+            self.number, 
+            self.hash(), 
+            self.previous_hash, 
+            self.data, 
+            self.nonce
+            )
+        )
+    
+    
+    
 class Blockchain():
-    pass
+    difficulty = 4
+    
+    def __init__(self):
+        self.chain = []
+
+    def add(self, block):
+        # self.chain.append({
+        #     'hash': block.hash(),
+        #     'previous': block.previous_hash,
+        #     'number': block.number,
+        #     'data': block.data,
+        #     'nonce': block.nonce
+        # })
+        self.chain.append(block)
+        
+    def remove(self, block):
+        self.chain.remove(block)
+        
+    def mine(self, block):
+        try:
+            block.previous_hash = self.chain[-1].hash()
+        except IndexError:
+            pass
+        
+        while True:
+            if block.hash()[:self.difficulty] == "0" * self.difficulty:
+                self.add(block); break
+            else:
+                block.nonce += 1
+                
+    def isValid(self):
+        for i in range(1, len(self.chain)):
+            _previous = self.chain[i].previous_hash
+            _current = self.chain[i-1].hash()
+            if _previous != _current or _current[:self.difficulty] != "0"*self.difficulty:
+                return False
+            
+        return True
 
 def main():
-    block = Block("Hello world", 1)
-    print(block.hash())
+    blockchain = Blockchain()
+    database = ["hello", "gday", "howzit", "seeya"]
 
-if __name__ == "__main__":
+    num = 0
+    for data in database:
+        num += 1
+        blockchain.mine(Block(data, num))
+        
+    for block in blockchain.chain:
+        print(block)
+        
+    print(blockchain.isValid())
+        
+if __name__ == '__main__':
     main()
+
